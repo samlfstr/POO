d2a1a73cd9d2628a4df646bf2429da94c1d38d6c
Samuel Foster <samlfstr@gmail.com>
2020-07-23 18:24:55 +0300

    update

--- Practice/Classes/FifthClass.php
+++ Practice/Classes/FifthClass.php
@@ -1,6 +1,5 @@
 <?php
 
-
 class Vehicle
 {
     var $color;
--- Practice/Classes/TenthClass.php
+++ Practice/Classes/TenthClass.php
@@ -8,7 +8,13 @@ class TenthClass
 
 class Person extends TenthClass{
 
+    // I am able to overwrite and when I do that a new var is created
+    public static $gender = null;
+    public static function modify(){
+        parent::$gender[] = "alien";
+    }
 }
 
-echo Person::$gender[] = 'shemale';
-var_dump(TenthClass::$gender);
+Person::modify(); // add alien to the main class
+var_dump(TenthClass::$gender); // all the elements of the main class
+var_dump(Person::$gender); // cleared, overwritten and null $gender variable
