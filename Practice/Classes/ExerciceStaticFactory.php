9a74ef7a1f9bca9da281d2092da170a4a802ec00
Samuel Foster <samlfstr@gmail.com>
2020-07-27 18:01:06 +0300

    'progress'

---
+++ Practice/Classes/ExerciceStaticFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+
+class ExerciceStaticFactory
+{
+    public static $info = "This is the information";
+
+    public static function create()
+    {
+         $class_name = get_called_class();
+         return new $class_name;
+    }
+}
+
+$instance = ExerciceStaticFactory::create();
+echo ExerciceStaticFactory::$info;;
