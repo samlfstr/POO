4b9bc29711210ee30366bbc8af2e89474a82353c
Samuel Foster <samlfstr@gmail.com>
2020-08-23 11:25:07 +0300

    Update

---
+++ Course/CMS/public/staff/index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+<head>
+ <meta charset="UTF-8">
+ <meta name="viewport"
+			 content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+ <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ <link rel="stylesheet" media="all" href="../stylesheets/staff.css">
+ <title>Document</title>
+</head>
+<body>
+<!--Header area-->
+<header>
+ <h1>GBI Staff Area</h1>
+</header>
+<!--END OF Header area-->
+
+<!--Navigation area-->
+<nav>
+ <ul>
+	<li><a href="index.php">Menu</a></li>
+ </ul>
+</nav>
+<!--END OF Navigation area-->
+
+<!--Content area-->
+<div id="content">
+
+</div>
+<!-- END OF Content area-->
+
+<!--Footer area-->
+<footer>
+ &copy; <?php echo date('Y') ?> Globe Bank
+</footer>
+<!--END OF Footer area-->
+</body>
+</html>
